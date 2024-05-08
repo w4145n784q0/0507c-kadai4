263136f00d1b47228db6c78e2b0896ef2b379ae0
直人 渡辺 <230080@jc-21.jp>
2024-05-08 15:28:20 +0900

    プロジェクト ファイルを追加します。

---
+++ 0507c++kadai4/Sample.h
@@ -0,0 +1,11 @@
+#pragma once
+class Sample
+{
+	//ƒƒ“ƒo•Ï”
+	int x;
+	//ƒƒ“ƒoŠÖ”
+public:
+	void Input1();
+	void Input2();
+	void Disp();
+};
---
+++ 0507c++kadai4/main.cpp
@@ -0,0 +1,13 @@
+#include "Sample.h"
+int main()
+{
+	int x = 30;
+	Sample s;
+	s.Input1();
+	s.Input2();
+	s.Disp();
+}
+
+//—\‘z 10(input2‚Ìx‚Íƒ[ƒJƒ‹•Ï”)
+
+//Œ‹‰Ê 10
